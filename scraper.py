35a08b608b3d4eb5fc5a841cc614eb479881de3a
Steven Karmaniolos <skarmaniolos@protonmail.com>
2019-05-02 17:35:32 +1000

    Update scraper.py
    fixed issue where any non-png file would cause error

--- scraper.py
+++ scraper.py
@@ -7,64 +7,73 @@ from io import BytesIO
 from PIL import Image
 
 
+# gets img from url and save to destination
 def download_file(url, dest_dir, filename=None):
     if(filename is None):
         filename = urlsplit(url).path.split("/")[-1]
-
+        
     request = requests.get(url)
     image = Image.open(BytesIO(request.content))
+    
+    if image.mode in ('RGBA', 'LA'):
+        #background = Image.new(image.mode[:-1], image.size, fill_color)
+        #background.paste(image, image.split()[-1])
+        background = img_alpha_to_colour(image)
+        image = background
+    
     image.save(os.path.join(dest_dir, filename))
 
-
+    
+# handles img that have no alpha layer 
+def img_alpha_to_colour(image, color=(255, 255, 255)):
+    image.load()  # needed for split()
+    background = Image.new('RGB', image.size, color)
+    background.paste(image, mask=image.split()[3])  # 3 is the alpha channel
+    return background
+    
+    
+# takes html as input, creates list, appends img elements to list
 def get_file_urls(soup):
     file_urls = []
-
-    # Fixes href errors which may occur
-    for anchor in soup.find_all(attrs={'class': 'fileThumb'}):
-        file_urls.append(sub("//", "http://", anchor.get('href')))
+    
+    # fix href errors
+    for anchor in soup.find_all(attrs={'class':'fileThumb'}):
+        file_urls.append(sub("//", "https://", anchor.get('href')))
     return file_urls
+    
 
-
+# iterate through posts, find img filenames
 def get_filenames(soup):
     filenames = []
 
     for anchor in soup.find_all(attrs={'class': 'fileText'}):
         filenames.append(anchor.get_text().split(" ")[1])
+        print(filenames.append(anchor.get_text().split(" ")[1]))
     return filenames
 
 
-def main():
+# primary function
+def main(url):
     print("┌─────────────────────────────────────────────────────────────────────┐")
-    print("│                         SUPER SCRAPER V0.1                          │")
     print("│           Image Scraping Tool built by Steven Karmaniolos           │")
     print("└─────────────────────────────────────────────────────────────────────┘")
+    print("Downloading images from " + url + " ...")
     
-    if(sys.argv[1] == "page"):
-        
-        # First argument is the page URL
-        url = sys.argv[2]
+    dest_dir = os.getcwd()
+    
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "html.parser")
 
-        # Second is the output directory
-        # Either relative path or abs. path
-        dest_dir = sys.argv[3]
-        print("Downloading images from " + url + " ...")
+    file_urls = get_file_urls(soup)
+    filenames = get_filenames(soup)
 
-        html = requests.get(url).text
-        soup = BeautifulSoup(html, "html.parser")
+    for i in range(len(file_urls)):
+        print("Downloading file: " + filenames[i])
+        download_file(file_urls[i], dest_dir, filenames[i])
 
-        file_urls = get_file_urls(soup)
-        filenames = get_filenames(soup)
-
-        for i in range(len(file_urls)):
-            print("Downloading file: " + filenames[i])
-            download_file(file_urls[i], dest_dir, filenames[i])
-        
-        print("┌─────────────────────────────────────────────────────────────────────┐")
-        print("│             Scrape Completed. Your files are now ready.             │")
-        print("└─────────────────────────────────────────────────────────────────────┘")
-
-    else:
-        print("Usage: python3 scraper.py <'page' or 'board'> <page url or board letter> <dest directory>")
+    print("┌─────────────────────────────────────────────────────────────────────┐")
+    print("│             Scrape completed. Your files are now ready.             │")
+    print("└─────────────────────────────────────────────────────────────────────┘")
 
 
 main()
